220f14ae526b9223974983d6231c46fe3a6ce9b7
shannon_p <snp1284kasey@aol.com>
2021-11-17 17:37:55 -0500

    added mutations and json web tokens

--- server/server.js
+++ server/server.js
@@ -2,12 +2,22 @@ const express = require('express');
 // import ApolloServer
 const {ApolloServer} = require("apollo-server-express");
 
+const path = require("path");
+
 // import our typesDefs and resolvers
 const {typeDefs, resolvers} = require("./schemas");
+const {authMiddleware} = require("./utils/auth");
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+// const server = new ApolloServer({ 
+//     typeDefs, 
+//     resolvers
+//     // context: authMiddleware 
+//   });
+
+//   server.applyMiddleware({ app });
 
 const startServer = async () => {
   // create a new Apollo server and pass in our schema data
@@ -36,5 +46,6 @@ app.use(express.json());
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
+    // console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
